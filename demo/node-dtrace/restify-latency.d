85c2d114d8c9c895aa3012a46a01bc5d9dd0b37e
Dave Pacheco <dap@joyent.com>
2013-12-13 18:58:04 -0800

    update restify latency script

--- demo/node-dtrace/restify-latency.d
+++ demo/node-dtrace/restify-latency.d
@@ -1,15 +1,27 @@
 #!/usr/sbin/dtrace -s
 #pragma D option quiet
 
-nodeconf*:::getn-*-start
+restify*:::route-start
 {
-  tracker[arg0, substr(probename, 0, rindex(probename, "-"))] = timestamp;
+   track[arg2] = timestamp;
 }
 
-nodeconf*:::getn-*-done
-/tracker[arg0, substr(probename, 0, rindex(probename, "-"))]/
+restify*:::handler-start
+/track[arg3]/
 {
-  this->name = substr(probename, 0, rindex(probename, "-"));
-  @[this->name] = quantize(((timestamp - tracker[arg0, this->name]) / 1000));
-  tracker[arg0, substr(probename, 0, rindex(probename, "-"))] = 0;
+   h[arg3, copyinstr(arg2)] = timestamp;
+}
+
+restify*:::handler-done
+/track[arg3] && h[arg3, copyinstr(arg2)]/
+{
+   @[copyinstr(arg2)] = quantize((timestamp - h[arg3, copyinstr(arg2)]) / 1000000);
+   h[arg3, copyinstr(arg2)] = 0;
+}
+
+restify*:::route-done
+/track[arg2]/
+{
+   @[copyinstr(arg1)] = quantize((timestamp - track[arg2]) / 1000000);
+   track[arg2] = 0;
 }
